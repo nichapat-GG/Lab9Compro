ca198a102ea4a6432238acc50c0231088f9d216b
Nichapat Kulfu <nichapat.farsai@gmail.com>
2025-12-19 15:32:20 +0700

    Update lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N,int M){
+	if(N <=0 || M <= 0){
+		cout << "Invalid input";
+	}
+
+	for(int i = 1;i <= N;i++){
+		for(int j = 1; j <= M; j++){
+			cout << "O";
+		}
+		cout << endl;
+	}
+}
 
 int main(){
 	
